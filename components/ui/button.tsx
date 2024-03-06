ff7c5715473ad5efe275f4fdccd15ad541c4f9aa
volfcan <20183360068@ogr.akdeniz.edu.tr>
2024-03-06 12:44:30 +0300

    final

---
+++ components/ui/button.tsx
@@ -0,0 +1,45 @@
+ import { cn } from "@/lib/utils";
+import {forwardRef} from "react"
+
+
+ export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+ 
+ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+ }, ref ) => {
+    return (
+        <button
+            className={cn(
+                `
+                rounded-full
+                w-auto
+                border-transparent
+                px-5
+                py-3
+                bg-black
+                disabled:cursor-not-allowed
+                disabled:opacity-50
+                text-white
+                hover:opacity-75
+                transition
+                `,
+                className
+            )}
+            disabled={disabled}
+            ref={ref}
+            {...props}
+            >
+              {children}
+        </button>
+    )
+ })
+
+
+Button.displayName = "Button";
+
+ export default Button;
