572802d5ee984b3c8d085f26d147b1d97c1d6ff3
volfcan <20183360068@ogr.akdeniz.edu.tr>
2024-03-26 13:15:40 +0300

    updates

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -1,25 +1,25 @@
-import { Billboard } from "@/types"
+import { Billboard } from "@/types";
 
 interface BillboardProps {
-    data: Billboard;
-};
-
-
-const Billboard: React.FC<BillboardProps> = ({
-    data
-    }) => { 
-        console
-    return (
-        <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-            <div className="rounded-xl bg-cover overflow-hidden relative aspect-square md:aspect-[2.4/1]" style={{ backgroundImage: `url(${data?.imageUrl})` }}>
-                <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-                    <div className="text-white font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-                        {data.label}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+  data: Billboard;
 }
+
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  console;
+  return (
+    <div className="overflow-hidden rounded-xl p-4 sm:p-6 lg:p-8">
+      <div
+        className="relative aspect-square overflow-hidden rounded-xl bg-cover md:aspect-[2.4/1]"
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+      >
+        <div className="flex h-full w-full flex-col items-center justify-center gap-y-8 text-center">
+          <div className="max-w-xs text-3xl font-bold text-white sm:max-w-xl sm:text-5xl lg:text-6xl">
+            {data.label}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Billboard;
